e042bc9c4795f216754f02b20e57ef90a320fa06
Lybrook <omoshlybrook@gmail.com>
2025-02-13 10:18:50 +0300

    Add function to count unique visit days based on input array

--- complete.js
+++ complete.js
@@ -111,3 +111,31 @@ function solution(A, B) {
 }
 
 console.log(solution(["CCOOOOOO", "BB00000", "DDOOOOOO"], [2, 7, 11]));
+
+function solution(visits) {
+    
+    if (!visits || visits.length === 0) return 0;
+  
+    const dayMap = {
+      "Mon": 0,
+      "Tue": 1,
+      "Wed": 2,
+      "Thu": 3,
+      "Fri": 4,
+      "Sat": 5,
+      "Sun": 6
+    };
+  
+    let cards = 0;
+    let prevDayValue = -1; 
+    visits.forEach(visit => {
+      let currentDayValue = dayMap[visit];
+      if (prevDayValue === -1 || currentDayValue <= prevDayValue) {
+        cards++;
+      }
+      prevDayValue = currentDayValue;
+    });
+  
+    return cards;
+  }
+  
